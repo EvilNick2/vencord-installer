7cacc469e917b20231a34e153eff78aca32a87a0
EvilNick2 <72648332+EvilNick2@users.noreply.github.com.>
2024-06-30 07:48:04 +0100

    Inital Commit

---
+++ README.md
@@ -0,0 +1,70 @@
+<h1 align="center">Vencord Installer</h1>
+
+<p align="center">
+  <img alt="Github top language" src="https://img.shields.io/github/languages/top/EvilNick2/vencord-installer?color=56BEB8">
+
+  <img alt="Github language count" src="https://img.shields.io/github/languages/count/EvilNick2/vencord-installer?color=56BEB8">
+
+  <img alt="Repository size" src="https://img.shields.io/github/repo-size/EvilNick2/vencord-installer?color=56BEB8">
+
+  <img alt="License" src="https://img.shields.io/github/license/EvilNick2/vencord-installer?color=56BEB8">
+</p>
+
+<p align="center">
+  <a href="#dart-about">About</a> &#xa0; | &#xa0; 
+  <a href="#sparkles-features">Features</a> &#xa0; | &#xa0;
+  <a href="#rocket-technologies">Technologies</a> &#xa0; | &#xa0;
+  <a href="#white_check_mark-requirements">Requirements</a> &#xa0; | &#xa0;
+  <a href="#checkered_flag-starting">Starting</a> &#xa0; | &#xa0;
+  <a href="#memo-license">License</a> &#xa0; | &#xa0;
+  <a href="https://github.com/EvilNick2" target="_blank">Author</a>
+</p>
+
+<br>
+
+## :dart: About
+
+Vencord Installer is a script designed to automate the installation of Vencord, a custom Discord client, along with a selection of user plugins. It simplifies the process of setting up Vencord with your preferred plugins, ensuring you have a personalized Discord experience.
+
+## :sparkles: Features
+
+:heavy_check_mark: Automated installation of Vencord.\
+:heavy_check_mark: Easy plugin management.\
+:heavy_check_mark: Update checks for the script itself.
+
+## :rocket: Technologies
+
+The following tools were used in this project:
+
+- [Bash](https://www.gnu.org/software/bash/)
+- [Git](https://git-scm.com/)
+- [Node.js](https://nodejs.org/en/)
+- [pnpm](https://pnpm.io/)
+
+## :white_check_mark: Requirements
+
+Before starting :checkered_flag:, you need to have [Git](https://git-scm.com) and [Node](https://nodejs.org/en/) installed.
+
+## :checkered_flag: Starting
+
+```bash
+# Clone this project
+$ git clone https://github.com/EvilNick2/vencord-installer
+
+# Access
+$ cd vencord-installer
+
+# Run the script
+$ ./vencord.sh
+```
+
+## :memo: License ##
+
+This project is under license from MIT. For more details, see the [LICENSE](LICENSE.md) file.
+
+
+Made with :heart: by <a href="https://github.com/EvilNick2" target="_blank">EvilNick2</a>
+
+&#xa0;
+
+<a href="#top">Back to top</a>
---
+++ vencord.sh
@@ -0,0 +1,139 @@
+#!/bin/bash
+
+currentVersion="1.0.0"
+
+declare default="\033[1;0m"
+declare yellow="\033[1;33m"
+declare cyan="\033[1;36m"
+declare green="\033[1;32m"
+declare red="\033[1;31m"
+
+GITHUB_USER="EvilNick2"
+GITHUB_REPO="vencord-installer"
+SCRIPT_NAME="vencord.sh"
+
+checkAndUpdateScript() {
+    echo -e "${cyan}Checking for updates...${default}"
+
+    latestRelease=$(curl -s "https://api.github.com/repos/$GITHUB_USER/$GITHUB_REPO/releases/latest")
+
+    latestVersion=$(echo "$latestRelease" | grep '"tag_name":' | sed -E 's/.*"([^"]+)".*/\1/')
+    downloadUrl=$(echo "$latestRelease" | grep '"browser_download_url":' | grep "$SCRIPT_NAME" | sed -E 's/.*"([^"]+)".*/\1/')
+
+    if [ ! -z "$latestVersion" ] && [ ! -z "$downloadUrl" ] && [ "$latestVersion" != "$currentVersion" ]; then
+        echo -e "${yellow}New version available: $latestVersion. Updating...${default}"
+
+        tempScript="temp_$SCRIPT_NAME"
+        curl -Lo "$tempScript" "$downloadUrl"
+
+        chmod +x "$tempScript"
+
+        mv "$tempScript" "$SCRIPT_NAME"
+
+        echo -e "${green}Update complete. Please rerun the script.${default}"
+        exit 0
+    else
+        echo -e "${green}You are running the latest version (${currentVersion}).${default}"
+    fi
+}
+
+checkAndUpdateScript
+
+downloadPlugins() {
+	declare userpluginsRepo=(
+		https://github.com/D3SOX/vencord-userplugins
+		https://github.com/ImpishMoxxie/SoundBoardLogger
+		https://github.com/pernydev/DontLeak
+		https://github.com/nyakowint/vcNarrator-custom
+		https://github.com/Syncxv/vc-timezones
+		https://github.com/ethan-davies/ToastNotificationsMerge/
+		https://github.com/nyakowint/replaceActivityTypes
+		https://github.com/Syncxv/vc-message-logger-enhanced
+	)
+
+	for pkg in ${userpluginsRepo[@]}; do
+		echo -e "${yellow}\n=================================="
+		echo -e " Downloading ${pkg} "
+		echo -e "==================================${default}"
+		sleep 1
+		git clone -q $pkg
+	done
+
+	declare userpluginsWget=(
+		https://raw.githubusercontent.com/domi-btnr/Vencord-Plugins/main/Keyboard-Sounds/keyboardSounds.ts
+		https://raw.githubusercontent.com/exhq/uwuifier-vencord/main/uwuifier.ts
+	)
+
+	for pkg in ${userpluginsWget[@]}; do
+		echo -e "${yellow}\n=================================="
+		echo -e " Downloading ${pkg} "
+		echo -e "==================================${default}"
+		sleep 1
+		curl -OSs $pkg
+	done
+}
+
+installVencord() {
+	git clone -q https://github.com/Vendicated/Vencord.git
+	cd Vencord
+
+	npm i -g pnpm
+	pnpm install
+	pnpm build
+
+	pnpm inject
+
+	cd src
+	mkdir userplugins
+	cd userplugins
+	downloadPlugins
+	pnpm build
+}
+
+main() {
+	declare installDir="$USERPROFILE\Documents"
+	
+	cd $installDir
+
+	if [ -d "$installDir\Vencord" ]; then
+		echo -en "${red}\nVencord folder already exists. Do you wish to reinstall? (Y/N)? ${default}"
+		old_stty_cfg=$(stty -g)
+		stty raw -echo
+		answer=$( while ! head -c 1 | grep -i '[ny]' ;do true ;done )
+		stty $old_stty_cfg
+		if [ "$answer" != "${answer#[Yy]}" ];then
+			echo -en "${green}\nReinstalling Vencord!""\n${default}"
+			rm -rf "$installDir\Vencord"
+			installVencord
+		else
+			echo -e "\nExiting."
+		fi
+	else
+		installVencord
+	fi
+
+	echo -e "${green}\nALL DONE! :)${default}"
+	echo -en "${green}Press enter to exit"'!\n'"${default}"
+	echo -en "${green}You can now relaunch discord.\n${default}"
+	
+	# pause execution
+	read -p "" opt
+	case $opt in
+		* ) exit;;
+	esac
+
+}
+
+echo -e "This file will download Vencord with custom userplugins. Vencord is against Discord TOS, but is rarely enforced."
+while true; do
+	echo -en 'Do you wish to continue? (Y/N)? '
+	old_stty_cfg=$(stty -g)
+	stty raw -echo
+	answer=$( while ! head -c 1 | grep -i '[ny]' ;do true ;done )
+	stty $old_stty_cfg
+	if [ "$answer" != "${answer#[Yy]}" ];then
+		main
+	else
+		exit
+	fi
+done
