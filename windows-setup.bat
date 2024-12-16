d152409b80d2baa69f9396b6b78e2b713e95f411
EvilNick2 <72648332+EvilNick2@users.noreply.github.com>
2024-12-16 09:33:27 +0000

    Updated windows setup to download vencord-installer and delete itself after running

--- windows-setup.bat
+++ windows-setup.bat
@@ -7,7 +7,13 @@ winget install -e --id Git.Git
 echo Installing Node.js...
 winget install -e --id OpenJS.NodeJS
 
+echo Downloading vencord-windows.sh...
+powershell -Command "Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/EvilNick2/vencord-installer/main/vencord-windows.sh' -OutFile 'vencord-windows.sh'"
+
 start "" "C:\Program Files\Git\git-bash.exe" -c "bash vencord-windows.sh"
+
+echo Deleting setup script...
+del "%~f0"
 
 endlocal
 exit
